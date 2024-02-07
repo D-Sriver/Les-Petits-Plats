279a8dece835b022d3bd596df09c223a13734798
D-Sriver <duez.s@proton.me>
2024-02-07 15:18:54 +0100

    ✨ Récupération de l'input

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
 // importation des fonctions à exécuter
+import { inputData } from './data/input-data.js';
 import { displayRecipes } from './Templates/recipe-card.js';
 import { displayRecipeCounter } from './utils/recipe-counter.js';
+
+
 // Appel des fonctions
 displayRecipes();
 displayRecipeCounter();
+inputData();
