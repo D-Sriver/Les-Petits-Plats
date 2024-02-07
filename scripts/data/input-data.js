fcc5dcfc0c9a8d9d76f84271a1c9a098992faa76
D-Sriver <duez.s@proton.me>
2024-02-07 19:18:20 +0100

    ✨ Ajoute la logique des Tag

--- scripts/data/input-data.js
+++ scripts/data/input-data.js
@@ -1,15 +1,20 @@
-export function inputData() {
-  const inputElement = document.getElementById('search');
-  const buttonElement = document.getElementById('button');
+export function addTag() {
+  const tagElement = document.querySelector(".tag");
+  const inputElement = document.getElementById("search");
+  const buttonElement = document.getElementById("button");
 
-  buttonElement.addEventListener('click', () => {
-    afficherValeurInputConsole(inputElement);
+  buttonElement.addEventListener("click", () => {
+    tagElement.style.display = "flex";
+    let height = 0;
+    const heightIntervalId = setInterval(() => {
+      if (height < 40) {
+        tagElement.style.height = height + "px";
+        height++;
+      } else {
+        clearInterval(heightIntervalId);
+      }
+    }, 1);
+
+    tagElement.textContent = inputElement.value;
   });
 }
-
-export function afficherValeurInputConsole(inputElement) {
-  const valeurInput = inputElement.value;
-  console.log(valeurInput);
-}
-
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 // importation des fonctions à exécuter
-import { inputData } from './data/input-data.js';
 import { displayRecipes } from './Templates/recipe-card.js';
+import { addTag } from './data/input-data.js';
 import { displayRecipeCounter } from './utils/recipe-counter.js';
 
 
 // Appel des fonctions
 displayRecipes();
 displayRecipeCounter();
-inputData();
+addTag();
