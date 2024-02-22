d8c4e682a952ed2b084867c72000a5677b72a38d
D-Sriver <duez.s@proton.me>
2024-02-22 14:59:43 +0100

    ♻️ Ajout variable recipe-counter

--- scripts/utils/recipe-counter.js
+++ scripts/utils/recipe-counter.js
@@ -2,10 +2,10 @@ import { recipes } from '../data/recipes.js';
 
 const recipeCounter = document.querySelector('.counter-recipe');
 const recipeTotal = document.querySelector('.counter-total');
-
+const addRecipe = 1450;
 const displayRecipeCounter = () => {
-  recipeCounter.textContent = `${recipes.length + 1450}`;
-  recipeTotal.textContent = `${recipes.length + 1450} recettes`;
+  recipeCounter.textContent = `${recipes.length + addRecipe}`;
+  recipeTotal.textContent = `${recipes.length + addRecipe} recettes`;
 }
 
 // Afficher le nombre total de recettes dans la console
